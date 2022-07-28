45d1f6363ab9e03b7e02d0561cc395444b4985dd
cherryday <tcarevmaxim@gmail.com>
2022-07-28 15:20:00 +0300

    add validation pipe

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,9 +1,17 @@
+import { IsNumber, IsString, Max, Min } from 'class-validator';
+
 export class CreateReviewDto {
+  @IsString()
   name: string;
 
+  @IsString()
   title: string;
 
+  @IsString()
   description: string;
 
+  @Max(5)
+  @Min(1)
+  @IsNumber()
   rating: number;
 }
